bd812c766690a673f2abd359f640fa4d1a63324d
marlonolmedo <jolmedo@informaticaylogistica.com>
2024-06-04 12:42:20 -0700

    change for analitics and speedcheck

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import './index.css'
 // import App from './App.svelte'
 import App from './Myapp.svelte'
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+// import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+inject();
+injectSpeedInsights();
 
 const app = new App({
   target: document.getElementById('app'),
